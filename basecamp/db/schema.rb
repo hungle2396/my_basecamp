2bde5b5945f4cb3bb472c7f3353c521fb999dedc
alekseevayana <alekseevayana@bk.ru>
2020-12-17 11:07:43 -0800

    add admin role

--- basecamp/db/schema.rb
+++ basecamp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_000026) do
+ActiveRecord::Schema.define(version: 2020_12_17_184118) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_12_12_000026) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
