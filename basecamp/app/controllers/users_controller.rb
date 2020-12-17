d39254b093aa65054236d0f230bbef1783f217ca
alekseevayana <alekseevayana@bk.ru>
2020-12-17 15:31:19 -0800

    link to all users from admin

--- basecamp/app/controllers/users_controller.rb
+++ basecamp/app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def index
+        @users = User.all
+    end
+
     def destroy
         @user = User.find(params[:id])
         @user.destroy
