b0f23d049ed5b4521b3edd86d23700ef61fab731
alekseevayana <alekseevayana@bk.ru>
2020-12-16 16:21:13 -0800

    destroy

--- basecamp/app/controllers/users_controller.rb
+++ basecamp/app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+
+        if @user.destroy
+            redirect_to root_url, notice: "User deleted."
+        end
+    end
 end
