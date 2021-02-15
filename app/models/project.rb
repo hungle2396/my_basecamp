ea31e1ab3071535dc61a2ade6ab7b47575644bfb
Hung Le <hungle2396@gmail.com>
2021-02-15 02:20:33 -0800

    redesign the login and signup form and etc

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
     validates :title, presence: true, length: { minimum: 3, maximum: 30 }
     validates_uniqueness_of :title
 
-    validates :description, presence: true, length: { minimum: 10, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 150 }
 
     has_many :groups, dependent: :destroy
     has_many :users, through: :groups
