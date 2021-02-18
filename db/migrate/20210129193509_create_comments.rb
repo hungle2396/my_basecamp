042c2250372bbab7871b9e94be9243c4782b6ac9
Hung Le <hungle2396@gmail.com>
2021-02-18 14:36:28 -0800

    remove the thread from migration

--- db/migrate/20210129193509_create_comments.rb
+++ db/migrate/20210129193509_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.text :body
       t.references :user, null: false, foreign_key: true
-      t.references :thread, null: false, foreign_key: true
 
       t.timestamps
     end
