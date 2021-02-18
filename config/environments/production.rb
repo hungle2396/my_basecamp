9436017d8915ab642d96ec83ebe9c86fc186f163
Hung Le <hungle2396@gmail.com>
2021-02-18 00:14:12 -0800

    add some changes to action_cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_cable.allowed_request_origins = ["https://basecamp-17.herokuapp.com/"]
 end
