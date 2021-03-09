cf28b73995f138b8dc321e7c5803d591d2f19a98
Hung Le <hungle2396@gmail.com>
2021-03-09 13:40:18 -0800

    get rid of byebug

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -13,7 +13,6 @@ class InvitationsController < ApplicationController
     end
 
     def create
-        byebug
         @project = Project.find(params[:project_id])
         user = User.find_by(email: params[:email])
 
@@ -23,7 +22,7 @@ class InvitationsController < ApplicationController
         elsif user.present?
             group = Group.new(user_id: user.id, project_id: @project.id)
             @permitions = params[:permitions]
-            p @permitions
+
             if @permitions != nil
                 if @permitions.include? "write"
                     group.can_write = true
