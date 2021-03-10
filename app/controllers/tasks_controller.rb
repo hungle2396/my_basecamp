976894f79ec3aafcfbed8a293dd49656c779ee22
alekseevayana <alekseevayana@bk.ru>
2021-03-10 14:45:23 -0800

    test find and find_by in tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,10 @@ class TasksController < ApplicationController
         @project_tasks = @project.tasks
         @this_user = Group.find_by(user_id: current_user.id)
         @groups = Group.where(project_id: @project.id)
-        @group = @project.groups.find(params[:project_id])
+        #@group = @project.groups.find(params[:project_id])
+        #puts "GROUP with find is #{@group.id}"
+        @group = @project.groups.find_by(project_id: params[:project_id], user_id: current_user.id)
+        #puts "GROUP with find by is #{@group.id}"
     end
 
     def create
@@ -28,7 +31,7 @@ class TasksController < ApplicationController
 
     def edit
         @project = Project.find(params[:project_id])
-        @group = @project.groups.find_by(params[:project_id])
+        @group = @project.groups.find_by(project_id: params[:project_id], user_id: current_user.id)
         if @group.can_update || @group.is_admin
             @task = Task.find(params[:task_id])
         else
